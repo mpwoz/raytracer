1d2e43b51c0635457cceb6944370f845bd2da5fa
Martin Wozniewicz <martin.woz@gmail.com>
2021-04-02 19:38:17 -0700

    checkpoint; adding/subtracting points works
    todo: losing ownership of tuples every time an operation is performed. Need to pass by ref or use a more primitive type that implements Copy trait?

---
+++ src/point.rs
@@ -0,0 +1,147 @@
+use std::ops::{Add, Neg, Sub};
+
+#[derive(PartialEq, Debug)]
+struct Tuple {
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64, /* whether this is a point (1) or vector (0) */
+}
+
+impl Add for Tuple {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl Sub for Tuple {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+impl Neg for Tuple {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Tuple::vector(0., 0., 0.) - self
+    }
+}
+
+impl Tuple {
+
+    fn point(x: f64, y: f64, z: f64) -> Tuple {
+        Tuple { x, y, z, w: 1.0 }
+    }
+
+    fn vector(x: f64, y: f64, z: f64) -> Tuple {
+        Tuple { x, y, z, w: 0.0 }
+    }
+
+    fn is_point(&self) -> bool {
+        self.w == 1.0
+    }
+
+    fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_point() {
+        let point = Tuple {
+            x: 4.3,
+            y: -4.2,
+            z: 3.1,
+            w: 1.0,
+        };
+
+        assert_eq!(point.x, 4.3);
+        assert_eq!(point.y, -4.2);
+        assert_eq!(point.z, 3.1);
+        assert_eq!(point.w, 1.0);
+        assert_eq!(point.is_point(), true);
+        assert_eq!(point.is_vector(), false);
+
+        assert_eq!(Tuple::point(4.3, -4.2, 3.1), point)
+    }
+
+    #[test]
+    fn test_vector() {
+        let vector = Tuple {
+            x: 4.3,
+            y: -4.2,
+            z: 3.1,
+            w: 0.0,
+        };
+
+        assert_eq!(vector.x, 4.3);
+        assert_eq!(vector.y, -4.2);
+        assert_eq!(vector.z, 3.1);
+        assert_eq!(vector.w, 0.0);
+        assert_eq!(vector.is_point(), false);
+        assert_eq!(vector.is_vector(), true);
+
+        assert_eq!(Tuple::vector(4.3, -4.2, 3.1), vector)
+    }
+
+    #[test]
+    fn test_equality_floating_points() {
+        // so precise
+        let p1 = Tuple::point(9.0, 1.00000000000000000000000000000001, 9999999999.9);
+        let p2 = Tuple::point(8.9999999999999999999999999999, 1.0, 9999999999.9);
+
+        assert_eq!(p1, p2)
+    }
+
+    #[test]
+    fn test_adding_two_tuples() {
+        let p = Tuple::point(3., -2., 5.);
+        let v = Tuple::vector(-2., 3., 1.);
+
+        assert_eq!(p + v, Tuple::point(1., 1., 6.))
+    }
+
+    #[test]
+    fn test_subtracting_two_points() {
+        let a = Tuple::point(3., 2., 1.);
+        let b = Tuple::point(5., 6., 7.);
+
+        assert_eq!(a - b, Tuple::vector(-2., -4., -6.))
+    }
+
+    #[test]
+    fn test_subtracting_two_vectors() {
+        let a = Tuple::vector(3., 2., 1.);
+        let b = Tuple::vector(5., 6., 7.);
+
+        assert_eq!(a - b, Tuple::vector(-2., -4., -6.))
+    }
+
+    // #[test]
+    // fn test_negating_vectors() {
+    //     let a = Tuple::vector(3., 2., 1.);
+    //     let b = -a;
+    //
+    //     assert_eq!(-a, Tuple::vector(-3., -2., -1.));
+    // }
+
+}
