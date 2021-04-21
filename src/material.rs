8efe8365f02ff3a92b6ac2bd229759dbbe9aff64
Martin Wozniewicz <martin.woz@gmail.com>
2021-04-21 10:19:49 -0700

    Default material properties that make sense.

--- src/material.rs
+++ src/material.rs
@@ -1,5 +1,9 @@
+use crate::assert_eqf64;
+use crate::color::Color;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Material {
+    pub color: Color,
     pub ambient: f64,
     pub diffuse: f64,
     pub specular: f64,
@@ -9,10 +13,28 @@ pub struct Material {
 impl Material {
     pub fn new() -> Material {
         Material {
-            ambient: 0.5,
-            diffuse: 0.5,
-            specular: 0.5,
-            shininess: 0.5,
+            color: Color::WHITE,
+            ambient: 0.1,
+            diffuse: 0.9,
+            specular: 0.9,
+            shininess: 200.0,
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_material() {
+        let m = Material::new();
+
+        assert_eq!(m.color, Color::rgb(1., 1., 1.));
+        assert_eqf64!(m.ambient, 0.1);
+        assert_eqf64!(m.diffuse, 0.9);
+        assert_eqf64!(m.specular, 0.9);
+        assert_eqf64!(m.shininess, 200.0);
     }
 }
